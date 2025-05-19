038650cb1759ea898ffb9e48d1d2b42763e6d4e5
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-20 01:09:50 +0530

    Connect buttons to actions

---
+++ scripts/game_complete.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_play_game_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/map.tscn")
+
+
+
+func _on_exit_btn_pressed() -> void:
+	get_tree().quit()
